a0193349eeaffcf6ab62ebd060321f04bc8d5b40
Tanishq Patil <trpatil@Tanishqs-MacBook-Pro.local>
2026-01-01 02:54:42 -0800

    adding

--- ci/run_px4_ros2.sh
+++ ci/run_px4_ros2.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-set -e
-
+set -euo pipefail
 echo "PX4 + ROS 2 TEST"
-
 export PX4_SIM_SPEED_FACTOR=2
 export ROS_DOMAIN_ID=0
+export HEADLESS=1
+PX4_DIR=~/workspace/PX4-Autopilot
+ROS_WS=~/workspace/ros2_ws
 
-# Build PX4
-cd ~/workspace/PX4-Autopilot
-HEADLESS=1 make px4_sitl gz_x500
-#Build ROS
-echo "Building ROS 2 workspace..."
+# build PX4
+echo "Building PX4..."
+cd "$PX4_DIR"
+make px4_sitl gz_x500
+#build ROS
+echo "Building ROS..."
 source /opt/ros/humble/setup.bash
-cd ~/workspace/ros2_ws
+cd "$ROS_WS"
 colcon build --symlink-install
+source install/setup.bash
 #start microxrcedds
-echo "Starting Micro XRCE DDS Agent..."
+echo "Starting microxrce agent..."
 MicroXRCEAgent udp4 -p 8888 &
 DDS_PID=$!
-
-#start SITL
-cd ~/workspace/PX4-Autopilot
-HEADLESS=1 make px4_sitl gz_x500 &
+echo "Starting PX4 Simulation..."
+cd "$PX4_DIR"
+make px4_sitl gz_x500 &
 PX4_PID=$!
-sleep 15
-source ~/workspace/ros2_ws/install/setup.bash
-# confirm ros topics
+#debug
+sleep 20
 ros2 topic list | tee /tmp/topics.txt
 grep -q "/fmu/out/sensor_combined" /tmp/topics.txt
-timeout 5 ros2 topic echo /fmu/out/sensor_combined
-
-kill $PX4_PID $DDS_PID
+timeout 10 ros2 topic echo /fmu/out/sensor_combined --once
+echo "Cleaning up..."
+kill $PX4_PID $DDS_PID || true
 echo "Basic PX4 + ROS 2 test ran succesfully"
